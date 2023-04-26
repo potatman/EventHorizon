efb9f6beb6db81d130d28dc103fefba4fa312563
Daniel P. Rossi <DR9885@gmail.com>
2023-04-25 17:15:01 -0700

    [BOND-5625] possible option

--- src/Insperex.EventHorizon.EventSourcing/Aggregates/AggregateMasstransitConsumer.cs
+++ src/Insperex.EventHorizon.EventSourcing/Aggregates/AggregateMasstransitConsumer.cs
@@ -8,8 +8,13 @@ using MassTransit;
 
 namespace Insperex.EventHorizon.EventSourcing.Aggregates
 {
+    public interface IMasstransitRequest : IRequest
+    {
+        public string StreamId { get; set; }
+    }
+
     public class AggregateMasstransitConsumer<TParent, T, TReq> : IConsumer<Batch<TReq>>
-        where TReq : class, IRequest
+        where TReq : class, IMasstransitRequest
         where TParent : class, IStateParent<T>, new()
         where T : class, IState
     {
@@ -23,7 +28,7 @@ namespace Insperex.EventHorizon.EventSourcing.Aggregates
         public async Task Consume(ConsumeContext<Batch<TReq>> context)
         {
             var reqs = context.Message
-                .Select(x => new Request("???", x.Message) { Id = x.RequestId.ToString(), })
+                .Select(x => new Request(x.Message.StreamId, x.Message) { Id = x.RequestId.ToString(), })
                 .ToArray();
             var reps = await _aggregator.HandleAsync(reqs, context.CancellationToken);
             var dict = reps.ToDictionary(x => x.RequestId);
