5f28ade1416312344bc0397a4177d78d7217bcaa
Daniel P. Rossi <DR9885@gmail.com>
2023-05-02 08:46:32 -0700

    fix InMemory Store to replace

--- src/Insperex.EventHorizon.EventStore.InMemory/Extensions/EventHorizonConfiguratorExtensions.cs
+++ src/Insperex.EventHorizon.EventStore.InMemory/Extensions/EventHorizonConfiguratorExtensions.cs
@@ -3,6 +3,7 @@ using Insperex.EventHorizon.Abstractions.Util;
 using Insperex.EventHorizon.EventStore.Interfaces.Factory;
 using Insperex.EventHorizon.EventStore.Locks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Insperex.EventHorizon.EventStore.InMemory.Extensions;
 
@@ -11,21 +12,30 @@ public static class EventHorizonConfiguratorExtensions
     public static EventHorizonConfigurator AddInMemorySnapshotStore(this EventHorizonConfigurator configurator)
     {
         AddInMemoryStore(configurator);
-        configurator.Collection.AddSingleton(typeof(ISnapshotStoreFactory<>), typeof(InMemoryEventStoreFactory<>));
+        configurator.Collection.Replace(ServiceDescriptor.Describe(
+            typeof(ISnapshotStoreFactory<>),
+            typeof(InMemoryEventStoreFactory<>),
+            ServiceLifetime.Singleton));
         return configurator;
     }
 
     public static EventHorizonConfigurator AddInMemoryLockStore(this EventHorizonConfigurator configurator)
     {
         AddInMemoryStore(configurator);
-        configurator.Collection.AddSingleton(typeof(ILockStoreFactory<>), typeof(InMemoryEventStoreFactory<>));
+        configurator.Collection.Replace(ServiceDescriptor.Describe(
+            typeof(ILockStoreFactory<>),
+            typeof(InMemoryEventStoreFactory<>),
+            ServiceLifetime.Singleton));
         return configurator;
     }
 
     public static EventHorizonConfigurator AddInMemoryViewStore(this EventHorizonConfigurator configurator)
     {
         AddInMemoryStore(configurator);
-        configurator.Collection.AddSingleton(typeof(IViewStoreFactory<>), typeof(InMemoryEventStoreFactory<>));
+        configurator.Collection.Replace(ServiceDescriptor.Describe(
+            typeof(IViewStoreFactory<>),
+            typeof(InMemoryEventStoreFactory<>),
+            ServiceLifetime.Singleton));
         return configurator;
     }
 
