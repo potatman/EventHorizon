e1caac72278c9cbb8bd5031f3c1c65dcc24ec27d
Daniel P. Rossi <DR9885@gmail.com>
2023-05-01 17:05:13 -0700

    TypeDictionary defensive fix

--- src/Insperex.EventHorizon.Abstractions/Util/AssemblyUtil.cs
+++ src/Insperex.EventHorizon.Abstractions/Util/AssemblyUtil.cs
@@ -11,13 +11,13 @@ namespace Insperex.EventHorizon.Abstractions.Util;
 public static class AssemblyUtil
 {
     private static readonly Assembly Assembly = Assembly.GetCallingAssembly() ?? Assembly.GetEntryAssembly();
-    
-    private static readonly Assembly[] AllAssemblies = DependencyContext.Default?.CompileLibraries
-        .Select(x =>
+
+    private static readonly ImmutableDictionary<string, Type> TypeDictionary = DependencyContext.Default?.CompileLibraries
+        .SelectMany(x =>
         {
             try
             {
-                return Assembly.Load(x.Name);
+                return Assembly.Load(x.Name).GetTypes();
             }
             catch (Exception)
             {
@@ -25,29 +25,26 @@ public static class AssemblyUtil
             }
         })
         .Where(x => x != null)
-        .ToArray();
+        .ToLookup(x => x)
+        .ToImmutableDictionary(x => x.Key.Name, x => x.Last());
 
     public static readonly string AssemblyName = Assembly.GetName().Name;
     public static readonly Version AssemblyVersion = Assembly.GetName().Version;
     public static readonly string AssemblyNameAndVersion = $"{AssemblyName}-{AssemblyVersion}";
 
-    public static readonly ImmutableDictionary<string, Type> TypeDictionary = AllAssemblies
-        .SelectMany(x => x.GetTypes())
-        .ToLookup(x => x.Name)
-        .ToImmutableDictionary(x => x.Key, x => x.Last());
-    
+
     public static readonly ImmutableDictionary<string, PropertyInfo[]> PropertyDict = TypeDictionary
         .Where(x => x.Value.GetInterface(nameof(IState)) != null || x.Value.GetInterface(nameof(IAction)) != null)
         .ToImmutableDictionary(x => x.Key, x => x.Value.GetProperties());
-    
+
     public static readonly ImmutableDictionary<string, Type> StateDict = TypeDictionary
         .Where(x => x.Value.GetInterface(nameof(IState)) != null)
         .ToImmutableDictionary(x => x.Key, x => x.Value);
-    
+
     public static readonly ImmutableDictionary<string, PropertyInfo[]> PropertyDictOfStates = PropertyDict
         .ToImmutableDictionary(x => x.Key, x => x.Value
             .Where(p => p.PropertyType.GetInterface(nameof(IState)) != null).ToArray());
-    
+
     public static readonly ImmutableDictionary<string, Type[]> SubStateDict = PropertyDictOfStates
         .ToImmutableDictionary(x => x.Key, x => x.Value.Select(s => s.PropertyType).ToArray());
 
