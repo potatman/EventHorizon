797f08afddd184b226b76594e8b436aa9b6c662f
Daniel P. Rossi <DR9885@gmail.com>
2023-05-02 02:33:19 -0700

    minor change

--- src/Insperex.EventHorizon.EventStore/Interfaces/IStateParent.cs
+++ src/Insperex.EventHorizon.EventStore/Interfaces/IStateParent.cs
@@ -1,8 +1,9 @@
-using System;
+using Insperex.EventHorizon.Abstractions.Interfaces;
 
 namespace Insperex.EventHorizon.EventStore.Interfaces;
 
 public interface IStateParent<T> : ICrudEntity
+    where T : IState
 {
     public long SequenceId { get; set; }
     public T State { get; set; }
