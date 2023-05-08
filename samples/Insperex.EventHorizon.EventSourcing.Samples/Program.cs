5c3fa7823d861c3388409901b6ce6599311275b3
Daniel P. Rossi <DR9885@gmail.com>
2023-05-08 04:00:13 -0700

    registering http endpoints for sender and aggregate

--- samples/Insperex.EventHorizon.EventSourcing.Samples/Program.cs
+++ samples/Insperex.EventHorizon.EventSourcing.Samples/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿using System.Globalization;
 using System.Threading.Tasks;
 using Insperex.EventHorizon.Abstractions.Extensions;
 using Insperex.EventHorizon.Abstractions.Models.TopicMessages;
@@ -13,9 +10,8 @@ using Insperex.EventHorizon.EventStore.ElasticSearch.Extensions;
 using Insperex.EventHorizon.EventStore.MongoDb.Extensions;
 using Insperex.EventHorizon.EventStreaming.Pulsar.Extensions;
 using Insperex.EventHorizon.EventStreaming.Subscriptions.Extensions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using MongoDB.Driver;
 using Serilog;
 
 namespace Insperex.EventHorizon.EventSourcing.Samples;
@@ -24,54 +20,115 @@ public class Program
 {
     static async Task Main(string[] args)
     {
-        var host = Host.CreateDefaultBuilder(args)
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddScoped<SearchAccountViewMiddleware>();
-                services.AddEventHorizon(x =>
-                {
-                    x.AddEventSourcing()
+        var opts = new WebApplicationOptions { EnvironmentName = "local" };
+        var builder = WebApplication.CreateBuilder(opts);
 
-                        // Hosted
-                        .ApplyRequestsToSnapshot<Account>()
-                        .ApplyEventsToView<SearchAccountView>(h=>
-                            h.UseMiddleware<SearchAccountViewMiddleware>())
+        builder.Host.UseSerilog((_, config) => { config.WriteTo.Console(formatProvider: CultureInfo.InvariantCulture); });
 
-                        .AddSubscription<AccountConsumer, Event>()
+        var services = builder.Services;
+        services.AddMvc();
+        services.AddEndpointsApiExplorer();
+        services.AddControllers();
+        services.AddSwaggerGen();
 
-                        // Stores
-                        .AddMongoDbSnapshotStore(hostContext.Configuration)
-                        .AddElasticViewStore(hostContext.Configuration)
-                        .AddPulsarEventStream(hostContext.Configuration);
-                });
-            })
-            .UseSerilog((_, config) => { config.WriteTo.Console(formatProvider: CultureInfo.InvariantCulture); })
-            .UseEnvironment("local")
-            .Build();
+        services.AddScoped<SearchAccountViewMiddleware>();
+        services.AddEventHorizon(x =>
+        {
+            x.AddEventSourcing()
 
-        await host.StartAsync();
+                // Hosted
+                .ApplyRequestsToSnapshot<Account>()
+                .ApplyEventsToView<SearchAccountView>(h =>
+                    h.UseMiddleware<SearchAccountViewMiddleware>())
 
-        // Send command and receive result
-        var client = host.Services.GetRequiredService<EventSourcingClient<Account>>();
+                .AddSubscription<AccountConsumer, Event>()
 
-        var result = await client.CreateSender()
-            .Timeout(TimeSpan.FromSeconds(120))
-            .GetErrorResult((status, error) =>
-            {
-                switch (status)
-                {
-                    case AggregateStatus.CommandTimedOut: return new AccountResponse(AccountResponseStatus.CommandTimedOut, error);
-                    case AggregateStatus.LoadSnapshotFailed: return new AccountResponse(AccountResponseStatus.LoadSnapshotFailed, error);
-                    case AggregateStatus.HandlerFailed: return new AccountResponse(AccountResponseStatus.HandlerFailed, error);
-                    case AggregateStatus.BeforeSaveFailed: return new AccountResponse(AccountResponseStatus.BeforeSaveFailed, error);
-                    case AggregateStatus.AfterSaveFailed: return new AccountResponse(AccountResponseStatus.AfterSaveFailed, error);
-                    case AggregateStatus.SaveSnapshotFailed: return new AccountResponse(AccountResponseStatus.SaveSnapshotFailed, error);
-                    case AggregateStatus.SaveEventsFailed: return new AccountResponse(AccountResponseStatus.SaveEventsFailed, error);
-                    default: throw new Exception($"Unhandled ResultStatus {status} => {error}");
-                }
-            })
-            .Build()
-            .SendAndReceiveAsync("123", new OpenAccount(100));
+                // Stores
+                .AddMongoDbSnapshotStore(builder.Configuration)
+                .AddElasticViewStore(builder.Configuration)
+                .AddPulsarEventStream(builder.Configuration);
+        });
+
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.MapEventSourcingEndpoints<Account>();
+        app.MapEventSourcingEndpoints<User>();
+
+        await app.RunAsync();
+
+
+
+        // var host = Host.CreateDefaultBuilder(args)
+        //     .ConfigureWebHostDefaults(x =>
+        //     {
+        //         x.ConfigureServices((hostContext, services) =>
+        //         {
+        //             services.AddMvc();
+        //             services.AddEndpointsApiExplorer();
+        //             services.AddControllers();
+        //             services.AddSwaggerGen();
+        //
+        //             services.AddScoped<SearchAccountViewMiddleware>();
+        //             services.AddEventHorizon(x =>
+        //             {
+        //                 x.AddEventSourcing()
+        //
+        //                     // Hosted
+        //                     .ApplyRequestsToSnapshot<Account>()
+        //                     .ApplyEventsToView<SearchAccountView>(h =>
+        //                         h.UseMiddleware<SearchAccountViewMiddleware>())
+        //
+        //                     .AddSubscription<AccountConsumer, Event>()
+        //
+        //                     // Stores
+        //                     .AddMongoDbSnapshotStore(hostContext.Configuration)
+        //                     .AddElasticViewStore(hostContext.Configuration)
+        //                     .AddPulsarEventStream(hostContext.Configuration);
+        //             });
+        //         });
+        //         x.Configure(app =>
+        //         {
+        //             app.UseSwagger();
+        //             app.UseSwaggerUI();
+        //             app.UseRouter(r =>
+        //             {
+        //
+        //                 r.MapGet("hello/{name}", (req, res, data) =>
+        //                 {
+        //                     return res.WriteAsync($"Hello, {data.Values["name"]}!");
+        //                 });
+        //             });
+        //         });
+        //     })
+        //
+        //     .UseSerilog((_, config) => { config.WriteTo.Console(formatProvider: CultureInfo.InvariantCulture); })
+        //     .UseEnvironment("local")
+        //     .Build();
+        //
+        // await host.RunAsync();
+
+        // // Send command and receive result
+        // var client = host.Services.GetRequiredService<EventSourcingClient<Account>>();
+        //
+        // var result = await client.CreateSender()
+        //     .Timeout(TimeSpan.FromSeconds(120))
+        //     .GetErrorResult((status, error) =>
+        //     {
+        //         switch (status)
+        //         {
+        //             case AggregateStatus.CommandTimedOut: return new AccountResponse(AccountResponseStatus.CommandTimedOut, error);
+        //             case AggregateStatus.LoadSnapshotFailed: return new AccountResponse(AccountResponseStatus.LoadSnapshotFailed, error);
+        //             case AggregateStatus.HandlerFailed: return new AccountResponse(AccountResponseStatus.HandlerFailed, error);
+        //             case AggregateStatus.BeforeSaveFailed: return new AccountResponse(AccountResponseStatus.BeforeSaveFailed, error);
+        //             case AggregateStatus.AfterSaveFailed: return new AccountResponse(AccountResponseStatus.AfterSaveFailed, error);
+        //             case AggregateStatus.SaveSnapshotFailed: return new AccountResponse(AccountResponseStatus.SaveSnapshotFailed, error);
+        //             case AggregateStatus.SaveEventsFailed: return new AccountResponse(AccountResponseStatus.SaveEventsFailed, error);
+        //             default: throw new Exception($"Unhandled ResultStatus {status} => {error}");
+        //         }
+        //     })
+        //     .Build()
+        //     .SendAndReceiveAsync("123", new OpenAccount(100));
 
     }
 }
---
+++ src/Insperex.EventHorizon.EventSourcing/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using Insperex.EventHorizon.Abstractions.Interfaces;
+using Insperex.EventHorizon.Abstractions.Interfaces.Actions;
+using Insperex.EventHorizon.Abstractions.Util;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Insperex.EventHorizon.EventSourcing.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void MapEventSourcingEndpoints<T>(this WebApplication app) where T : class, IState, new()
+        {
+            var type = typeof(T);
+
+            // Map Get
+            var esClient = app.Services.GetRequiredService<EventSourcingClient<T>>();
+            var aggregator = esClient.Aggregator().Build();
+            app.MapGet(type.Name + "/{id}", async (string id) =>
+                {
+                    var response = await aggregator.GetAggregateFromStateAsync(id, CancellationToken.None);
+                    return Results.Ok(response.State);
+                })
+                .WithTags(type.Name);
+
+            // Map Requests
+            var requests = AssemblyUtil.ActionDict.Values
+                .Where(x => x.GetInterfaces().Any(i => i.Name == typeof(IRequest<,>).Name &&  i.GetGenericArguments()[0] == type))
+                .ToDictionary(x => x, x => x.GetInterfaces().FirstOrDefault(i => i.Name == typeof(IRequest<,>).Name));
+            var methodReq = typeof(WebApplicationExtensions).GetMethod("MapRequest", BindingFlags.Static | BindingFlags.NonPublic);
+            foreach (var request in requests)
+            {
+                var requestArgs = request.Value.GetGenericArguments();
+                var generic = methodReq?.MakeGenericMethod(request.Key, requestArgs[1], requestArgs[0]);
+                generic?.Invoke(obj: null, parameters: new object[] { app });
+            }
+
+            // Map Commands
+            var commands = AssemblyUtil.ActionDict.Values
+                .Where(x => x.GetInterfaces().Any(i => i.Name == typeof(ICommand<>).Name &&  i.GetGenericArguments()[0] == type))
+                .ToDictionary(x => x, x => x.GetInterfaces().FirstOrDefault(i => i.Name == typeof(ICommand<>).Name));
+            var methodCmd = typeof(WebApplicationExtensions).GetMethod("MapCommand", BindingFlags.Static | BindingFlags.NonPublic);
+            foreach (var command in commands)
+            {
+                var requestArgs = command.Value.GetGenericArguments();
+                var generic = methodCmd?.MakeGenericMethod(command.Key, requestArgs[0]);
+                generic?.Invoke(obj: null, parameters: new object[] { app });
+            }
+        }
+
+        private static void MapRequest<TReq, TRes, T>(this WebApplication app)
+            where T : class, IState, new()
+            where TReq : IRequest<T, TRes>
+            where TRes : class, IResponse<T>
+        {
+            var esClient = app.Services.GetRequiredService<EventSourcingClient<T>>();
+            var sender = esClient.CreateSender().Build();
+            var typeName = typeof(T).Name;
+            var reqName = typeof(TReq).Name;
+            app.MapPost(typeName + "/{id}/" + reqName, async (string id, TReq req)  =>
+                {
+                    var response = await sender.SendAndReceiveAsync(id, req);
+                    return Results.Ok(response);
+                })
+                .WithTags(typeName);
+        }
+
+        private static void MapCommand<TCmd, T>(this WebApplication app)
+            where T : class, IState, new()
+            where TCmd : ICommand<T>
+        {
+            var sender = app.Services.GetRequiredService<EventSourcingClient<T>>().CreateSender().Build();
+
+            var typeName = typeof(T).Name;
+            var reqName = typeof(TCmd).Name;
+            app.MapPost(typeName + "/{id}/" + reqName, async (string id, TCmd cmd)  =>
+                {
+                    try
+                    {
+                        await sender.SendAsync(id, cmd);
+                        return Results.Ok();
+                    }
+                    catch (Exception e)
+                    {
+                        return Results.Conflict(e);
+                    }
+                })
+                .WithTags(typeName);
+        }
+    }
+}
