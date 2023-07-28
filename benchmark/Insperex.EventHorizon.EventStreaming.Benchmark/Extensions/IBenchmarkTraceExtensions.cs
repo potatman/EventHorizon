dbfbaaea3997003ef050466c969879efce347f1c
Zach Blocker <zblocker@insperex.com>
2023-07-28 09:57:18 -0400

    Adapter for NBench Trace as XUnit output

---
+++ benchmark/Insperex.EventHorizon.EventStreaming.Benchmark/Extensions/IBenchmarkTraceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Insperex.EventHorizon.EventStreaming.Benchmark.Utilities;
+using NBench;
+using Xunit.Abstractions;
+
+namespace Insperex.EventHorizon.EventStreaming.Benchmark.Extensions;
+
+public static class IBenchmarkTraceExtensions
+{
+    public static ITestOutputHelper AsXunitOutput(this IBenchmarkTrace trace)
+    {
+        return new NBenchTraceToXUnitOutputAdapter(trace);
+    }
+}
---
+++ benchmark/Insperex.EventHorizon.EventStreaming.Benchmark/Utilities/NBenchTraceToXUnitOutputAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using NBench;
+using Xunit.Abstractions;
+
+namespace Insperex.EventHorizon.EventStreaming.Benchmark.Utilities;
+
+public class NBenchTraceToXUnitOutputAdapter: ITestOutputHelper
+{
+    private readonly IBenchmarkTrace _trace;
+
+    public NBenchTraceToXUnitOutputAdapter(IBenchmarkTrace trace)
+    {
+        _trace = trace;
+    }
+
+    public void WriteLine(string message)
+    {
+        _trace.Info(message);
+    }
+
+    public void WriteLine(string format, params object[] args)
+    {
+        var formattedMessage = string.Format(CultureInfo.InvariantCulture, format ?? "", args);
+        _trace.Info(formattedMessage);
+    }
+}
