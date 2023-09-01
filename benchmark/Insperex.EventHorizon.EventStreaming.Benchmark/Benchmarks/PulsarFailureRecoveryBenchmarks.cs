54dd1407b90304669da7d012ba2a653d5088435b
Zach Blocker <zblocker@insperex.com>
2023-09-01 08:51:35 -0400

    Add benchmark for basic failures

--- benchmark/Insperex.EventHorizon.EventStreaming.Benchmark/Benchmarks/PulsarFailureRecoveryBenchmarks.cs
+++ benchmark/Insperex.EventHorizon.EventStreaming.Benchmark/Benchmarks/PulsarFailureRecoveryBenchmarks.cs
@@ -19,7 +19,7 @@ namespace Insperex.EventHorizon.EventStreaming.Benchmark.Benchmarks;
 
 public class PulsarFailureRecoveryBenchmarks
 {
-    private const int InputEventCount = 5_000;
+    private const int InputEventCount = 1_000;
     private const int BatchSize = 100;
     private const int IterationsPerScenario = 3;
 
@@ -55,98 +55,166 @@ public class PulsarFailureRecoveryBenchmarks
             CancellationToken.None).Wait();
     }
 
-    [PerfBenchmark(Description = "Pulsar consumer with no failures", Skip = "Not run by default",
+    [PerfBenchmark(Description = "Pulsar consumer with no failures (basic)", Skip = "Not run by default",
         NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
     [TimingMeasurement]
     [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
-    public void Single_consumer_with_no_failures(BenchmarkContext context)
+    public void Single_consumer_with_no_failures_basic(BenchmarkContext context)
+    {
+        RunFailureScenario_with_no_failures(context, false);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with no failures (advanced)", Skip = "Not run by default",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_no_failures_advanced(BenchmarkContext context)
+    {
+        RunFailureScenario_with_no_failures(context, true);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with very light level of failures (basic)", Skip = "Not run by default",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_very_light_failures_basic(BenchmarkContext context)
+    {
+        RunFailureScenario_with_very_light_failures(context, false);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with very light level of failures (advanced)", Skip = "Not run by default",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_very_light_failures_advanced(BenchmarkContext context)
+    {
+        RunFailureScenario_with_very_light_failures(context, true);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with light level of failures (basic)", Skip = "Not run by default",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_light_failures_basic(BenchmarkContext context)
+    {
+        RunFailureScenario_with_light_failures(context, false);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with light level of failures (advanced)", Skip = "Not run by default",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_light_failures_advanced(BenchmarkContext context)
+    {
+        RunFailureScenario_with_light_failures(context, true);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with moderate level of failures (basic)", Skip = "Not run by default",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement,
+        SkipWarmups = true)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_moderate_failures_basic(BenchmarkContext context)
+    {
+        RunFailureScenario_with_moderate_failures(context, false);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with moderate level of failures (advanced)", Skip = "Not run by default",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement,
+        SkipWarmups = true)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_moderate_failures_advanced(BenchmarkContext context)
+    {
+        RunFailureScenario_with_moderate_failures(context, true);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with heavy level of failures (basic)",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement,
+        SkipWarmups = true)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_heavy_failures_basic(BenchmarkContext context)
+    {
+        RunFailureScenario_with_heavy_failures(context, false);
+    }
+
+    [PerfBenchmark(Description = "Pulsar consumer with heavy level of failures (advanced)",
+        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement,
+        SkipWarmups = true)]
+    [TimingMeasurement]
+    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+    public void Single_consumer_with_heavy_failures_advanced(BenchmarkContext context)
+    {
+        RunFailureScenario_with_heavy_failures(context, true);
+    }
+
+    private void RunFailureScenario_with_no_failures(BenchmarkContext context, bool useAdvancedFailureHandling)
     {
         var handler = new ListStreamConsumer<Event>();
 
-        RunFailureScenario(context, handler,
-            () => InputEventCount <= handler.List.Count)
+        RunFailureScenario(context, handler, useAdvancedFailureHandling,
+                () => InputEventCount <= handler.List.Count)
             .GetAwaiter().GetResult();
     }
 
-    [PerfBenchmark(Description = "Pulsar consumer with very light level of failures", Skip = "Not run by default",
-        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
-    [TimingMeasurement]
-    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
-    public void Single_consumer_with_very_light_failures(BenchmarkContext context)
+    private void RunFailureScenario_with_very_light_failures(BenchmarkContext context, bool useAdvancedFailureHandling)
     {
         var handler = new PartialNackListStreamConsumer(context.Trace.AsXunitOutput(),
             0.005, 1, 1, 50, false);
 
-        RunFailureScenario(context, handler,
-            () => InputEventCount <= handler.List.Count)
+        RunFailureScenario(context, handler, useAdvancedFailureHandling,
+                () => InputEventCount <= handler.List.Count)
             .GetAwaiter().GetResult();
 
         handler.Report();
     }
 
-    [PerfBenchmark(Description = "Pulsar consumer with light level of failures", Skip = "Not run by default",
-        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
-    [TimingMeasurement]
-    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
-    public void Single_consumer_with_light_failures(BenchmarkContext context)
+    private void RunFailureScenario_with_light_failures(BenchmarkContext context, bool useAdvancedFailureHandling)
     {
         var handler = new PartialNackListStreamConsumer(context.Trace.AsXunitOutput(),
             0.03, 3, 2, 100, false);
 
-        RunFailureScenario(context, handler,
-            () => InputEventCount <= handler.List.Count)
+        RunFailureScenario(context, handler, useAdvancedFailureHandling,
+                () => InputEventCount <= handler.List.Count)
             .GetAwaiter().GetResult();
 
         handler.Report();
     }
 
-    [PerfBenchmark(Description = "Pulsar consumer with moderate level of failures", Skip = "Not run by default",
-        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement,
-        SkipWarmups = true)]
-    [TimingMeasurement]
-    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
-    public void Single_consumer_with_moderate_failures(BenchmarkContext context)
+    private void RunFailureScenario_with_moderate_failures(BenchmarkContext context, bool useAdvancedFailureHandling)
     {
         var handler = new PartialNackListStreamConsumer(context.Trace.AsXunitOutput(),
             0.1, 8, 4, 1000, false);
 
-        RunFailureScenario(context, handler,
-            () => InputEventCount <= handler.List.Count)
+        RunFailureScenario(context, handler, useAdvancedFailureHandling,
+                () => InputEventCount <= handler.List.Count)
             .GetAwaiter().GetResult();
 
         handler.Report();
     }
 
-    [PerfBenchmark(Description = "Pulsar consumer with heavy level of failures", Skip = "Not run by default",
-        NumberOfIterations = IterationsPerScenario, RunMode = RunMode.Iterations, TestMode = TestMode.Measurement,
-        SkipWarmups = true)]
-    [TimingMeasurement]
-    [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
-    public void Single_consumer_with_heavy_failures(BenchmarkContext context)
+    private void RunFailureScenario_with_heavy_failures(BenchmarkContext context, bool useAdvancedFailureHandling)
     {
         var handler = new PartialNackListStreamConsumer(context.Trace.AsXunitOutput(),
             0.6, 20, 6, 2000, false);
 
-        RunFailureScenario(context, handler,
-            () => InputEventCount <= handler.List.Count)
+        RunFailureScenario(context, handler, useAdvancedFailureHandling,
+                () => InputEventCount <= handler.List.Count)
             .GetAwaiter().GetResult();
 
         handler.Report();
     }
 
     private async Task RunFailureScenario(BenchmarkContext context, IStreamConsumer<Event> handler,
-        Func<bool> endCondition)
+        bool useAdvancedFailureHandling, Func<bool> endCondition)
     {
         // Consume
-        await using var subscription = await _streamingClient.CreateSubscription<Event>()
+        var builder = _streamingClient.CreateSubscription<Event>()
             .SubscriptionName($"FailBenchmark_{UniqueTestId}")
             .SubscriptionType(SubscriptionType.KeyShared)
             .AddStream<Feed1PriceChanged>()
             .BatchSize(BatchSize)
-            .GuaranteeMessageOrderOnFailure(true)
-            .ExponentialBackoff(b => b
-                .StartAt(TimeSpan.FromMilliseconds(10))
-                .Max(TimeSpan.FromSeconds(8)))
             .OnBatch(async ctx =>
             {
                 try
@@ -158,7 +226,23 @@ public class PulsarFailureRecoveryBenchmarks
                     context.Trace.Error(e, e.Message);
                     throw;
                 }
-            })
+            });
+
+        if (useAdvancedFailureHandling)
+        {
+            builder
+                .GuaranteeMessageOrderOnFailure(true)
+                .ExponentialBackoff(b => b
+                    .StartAt(TimeSpan.FromMilliseconds(10))
+                    .Max(TimeSpan.FromSeconds(8)));
+        }
+        else
+        {
+            builder
+                .FailedMessageRedeliveryDelay(TimeSpan.FromMilliseconds(5));
+        }
+
+        await using var subscription = await builder
             .Build()
             .StartAsync();
 
