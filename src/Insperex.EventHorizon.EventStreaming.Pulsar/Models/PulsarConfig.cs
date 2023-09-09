b75e066c007b8ecbeb3fe69d9eb88d3003ec45ca
Daniel P. Rossi <DR9885@gmail.com>
2023-09-09 07:43:11 -0500

    Adding OAuth2 to pulsar client

--- src/Insperex.EventHorizon.EventStreaming.Pulsar/Models/PulsarConfig.cs
+++ src/Insperex.EventHorizon.EventStreaming.Pulsar/Models/PulsarConfig.cs
@@ -4,4 +4,14 @@ public class PulsarConfig
 {
     public string ServiceUrl { get; set; }
     public string AdminUrl { get; set; }
+
+    public PulsarOAuth2Config OAuth2 { get; set; }
+}
+
+public class PulsarOAuth2Config
+{
+    public string File { get; set; }
+    public string Audience { get; set; }
+    public string TokenAddress { get; set; }
+    public string GrantType { get; set; }
 }
