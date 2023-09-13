1130f2456a147b24a237bf84452151681165c440
Daniel P. Rossi <DR9885@gmail.com>
2023-09-12 23:41:51 -0500

    add option IsOrderGuaranteed to optimize writes if needed

--- src/Insperex.EventHorizon.EventStreaming/Publishers/PublisherConfig.cs
+++ src/Insperex.EventHorizon.EventStreaming/Publishers/PublisherConfig.cs
@@ -5,6 +5,7 @@ namespace Insperex.EventHorizon.EventStreaming.Publishers;
 public class PublisherConfig
 {
     public string Topic { get; set; }
+    public bool IsOrderGuaranteed { get; set; }
     public bool IsGuaranteed { get; set; }
     public TimeSpan SendTimeout { get; set; }
     public int BatchSize { get; set; }
