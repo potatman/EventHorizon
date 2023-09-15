e9baab63797f5e85264797172f4c2da047469a07
Daniel P. Rossi <DR9885@gmail.com>
2023-09-15 07:59:27 -0500

    Adding lock logs

--- src/Insperex.EventHorizon.EventStore/Locks/LockFactory.cs
+++ src/Insperex.EventHorizon.EventStore/Locks/LockFactory.cs
@@ -3,26 +3,29 @@ using Insperex.EventHorizon.Abstractions.Interfaces;
 using Insperex.EventHorizon.EventStore.Interfaces.Factory;
 using Insperex.EventHorizon.EventStore.Interfaces.Stores;
 using Insperex.EventHorizon.EventStore.Models;
+using Microsoft.Extensions.Logging;
 
 namespace Insperex.EventHorizon.EventStore.Locks;
 
 public class LockFactory<T> where T : class, IState
 {
+    private readonly ILoggerFactory _loggerFactory;
     private readonly ICrudStore<Lock> _lockStore;
 
-    public LockFactory(ILockStoreFactory<T> lockStore)
+    public LockFactory(ILockStoreFactory<T> lockStore, ILoggerFactory loggerFactory)
     {
+        _loggerFactory = loggerFactory;
         _lockStore = lockStore.GetLockStore();
     }
 
     public LockDisposable CreateLock(string id)
     {
-        return new LockDisposable(_lockStore, id, TimeSpan.FromMinutes(5));
+        return new LockDisposable(_lockStore, id, TimeSpan.FromMinutes(5), _loggerFactory.CreateLogger<LockDisposable>());
     }
 
     public LockDisposable CreateLock(string id, TimeSpan timeout)
     {
         // Create Lock
-        return new LockDisposable(_lockStore, id, timeout);
+        return new LockDisposable(_lockStore, id, timeout, _loggerFactory.CreateLogger<LockDisposable>());
     }
 }
