8d2336f29f37ffbb3f2647162328eb5d4d2134a8
Daniel P. Rossi <DR9885@gmail.com>
2023-09-15 08:26:22 -0500

    adding owner to lock

--- src/Insperex.EventHorizon.EventStore/Models/Lock.cs
+++ src/Insperex.EventHorizon.EventStore/Models/Lock.cs
@@ -7,6 +7,7 @@ public class Lock : ICrudEntity
 {
     public DateTime Expiration { get; set; }
     public string Id { get; set; }
+    public string Owner { get; set; }
     public DateTime UpdatedDate { get; set; }
     public DateTime CreatedDate { get; set; }
 }
