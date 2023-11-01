927eeb5d0a69e0dbbac7ab4211b4102ab224a26d
Daniel P. Rossi <DR9885@gmail.com>
2023-11-01 15:50:29 -0500

    sample change

--- samples/Insperex.EventHorizon.EventSourcing.Samples/Subscriptions/AccountConsumer.cs
+++ samples/Insperex.EventHorizon.EventSourcing.Samples/Subscriptions/AccountConsumer.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Insperex.EventHorizon.Abstractions.Models.TopicMessages;
+using Insperex.EventHorizon.EventStreaming.Extensions;
 using Insperex.EventHorizon.EventStreaming.Interfaces.Streaming;
 using Insperex.EventHorizon.EventStreaming.Subscriptions;
 
@@ -10,6 +12,7 @@ public class AccountConsumer : IStreamConsumer<Event>
     public Task OnBatch(SubscriptionContext<Event> context)
     {
         // TODO: Handle Subscription
+        var events = context.Messages.Select(x => x.Data.GetPayload()).ToArray();
         return Task.CompletedTask;
     }
 }
