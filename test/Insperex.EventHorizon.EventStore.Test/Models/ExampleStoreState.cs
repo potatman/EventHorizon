5fe64f3ce78d962e90eb30cc86413415f35600a2
Daniel P. Rossi <DR9885@gmail.com>
2024-03-12 09:56:06 -0500

    More fake data

--- test/Insperex.EventHorizon.EventStore.Test/Models/ExampleStoreState.cs
+++ test/Insperex.EventHorizon.EventStore.Test/Models/ExampleStoreState.cs
@@ -10,4 +10,21 @@ public class ExampleStoreState : IState
 {
     public string Id { get; set; }
     public string Name { get; set; }
+
+    public ExampleSubclass[] SubClasses { get; set; }
+
+}
+
+public class ExampleSubclass
+{
+    public string Name1 { get; set; }
+    public string Name2 { get; set; }
+    public string Name3 { get; set; }
+    public string Name4 { get; set; }
+    public string Name5 { get; set; }
+    public string Name6 { get; set; }
+    public string Name7 { get; set; }
+    public string Name8 { get; set; }
+    public string Name9 { get; set; }
+    public string Name10 { get; set; }
 }
