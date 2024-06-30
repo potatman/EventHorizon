50eabd395379ff7738a7e50b2a27bbdae3e6dad1
Prescott Nasser <=>
2024-06-29 23:12:46 -0700

    Simplifying Pulsar Credentials
    The Pulsar F# library takes credentials from a Uri. The library however can handle two types of Uri Schemes:
    
    - File: This tells the F# library to load a file from a given Uri.
    - Data: This tells the F# library that the Uri is actually a Base64 encoded JSON configuration object
    
    EventHorizons originally used File. This PR changes that to use Data.
    
    The original File implementation had a number of flaws that were painful:
      - We created an oauth2.txt file everytime the PulsarClientResolver was called; this incurred IO
      - We deleted the file on dispose, that created multi-threading conflicts in several places
      - The code was much more complex to handle this

--- src/Insperex.EventHorizon.EventStreaming.Pulsar/PulsarClientResolver.cs
+++ src/Insperex.EventHorizon.EventStreaming.Pulsar/PulsarClientResolver.cs
@@ -1,117 +1,112 @@
 using System;
-using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using IdentityModel.Client;
+using Insperex.EventHorizon.EventStreaming.Pulsar.Extensions;
 using Insperex.EventHorizon.EventStreaming.Pulsar.Models;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using Pulsar.Client.Api;
 using SharpPulsar.Admin.v2;
 
-namespace Insperex.EventHorizon.EventStreaming.Pulsar
+namespace Insperex.EventHorizon.EventStreaming.Pulsar;
+
+public class PulsarClientResolver : IDisposable
 {
-    public class PulsarClientResolver : IDisposable
+    private IOptions<PulsarConfig> _options;
+    private PulsarAdminRESTAPIClient _admin;
+    private PulsarClient _client;
+    private Uri _credentials;
+    private bool disposed;
+
+    public PulsarClientResolver(IOptions<PulsarConfig> options)
     {
-        private readonly IOptions<PulsarConfig> _options;
-        private PulsarAdminRESTAPIClient _admin;
-        private PulsarClient _client;
-        private readonly Uri _fileUri;
-        private readonly string _fileName;
+        _options = options;
+        _credentials = _options.Value.OAuth2.ToBase64EncodedUri();
+    }
 
-        public PulsarClientResolver(IOptions<PulsarConfig> options)
+    public async Task<PulsarClient> GetPulsarClientAsync()
+    {
+        if (_client != null)
+            return _client;
+
+        var builder = new PulsarClientBuilder()
+            .ServiceUrl(_options.Value.ServiceUrl)
+            .EnableTransaction(true);
+
+        if (_options.Value.OAuth2 != null)
         {
-            _options = options;
-
-            // Create File for pulsar client
-            _fileName = $"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}oauth2.txt";
-            var json = JsonConvert.SerializeObject(_options.Value.OAuth2);
-
-            if(!File.Exists(_fileName))
-                File.WriteAllText(_fileName, json);
-
-            _fileUri = new Uri(_fileName);
+            var audience = _options.Value.OAuth2.Audience;
+            builder = builder.Authentication(AuthenticationFactoryOAuth2.ClientCredentials(new Uri(_options.Value.OAuth2.IssuerUrl), audience, _credentials));
         }
 
-        public async Task<PulsarClient> GetPulsarClientAsync()
+        return await builder.BuildAsync();
+    }
+
+    public async Task<IPulsarAdminRESTAPIClient> GetAdminClientAsync()
+    {
+        if (_admin != null) return _admin;
+
+        var client = new HttpClient
         {
-            if (_client != null)
-                return _client;
+            BaseAddress = new Uri($"{_options.Value.AdminUrl}/admin/v2/")
+        };
 
-            var builder = new PulsarClientBuilder()
-                .ServiceUrl(_options.Value.ServiceUrl)
-                .EnableTransaction(true);
+        if (_options.Value.OAuth2 != null)
+        {
+            var token = await GetTokenAsync(_options.Value.OAuth2);
+            client.SetBearerToken(token);
+        }
 
-            if (_options.Value.OAuth2 != null)
+        return _admin = new PulsarAdminRESTAPIClient(client);
+    }
+
+    public HttpClient GetAdminHttpClient()
+    {
+        return new HttpClient { BaseAddress = new Uri($"{_options.Value.AdminUrl}/admin/v2/") };
+    }
+
+    private static async Task<string> GetTokenAsync(PulsarOAuth2Config config)
+    {
+        var request = new TokenRequest
+        {
+            Address = config.TokenAddress,
+            GrantType = config.GrantType,
+            ClientId = config.ClientId,
+            ClientSecret = config.ClientSecret,
+            ClientCredentialStyle = ClientCredentialStyle.PostBody,
+        };
+        request.Parameters.Add("audience", config.Audience);
+        request.Parameters.Add("type", config.Type);
+        request.Parameters.Add("client_email", config.ClientEmail);
+        request.Parameters.Add("issuer_url", config.IssuerUrl);
+
+        // Get Token
+        var client = new HttpClient();
+        var response = await client.RequestTokenAsync(request);
+        return response.AccessToken;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
             {
-                var audience = _options.Value.OAuth2.Audience;
-                builder = builder.Authentication(AuthenticationFactoryOAuth2.ClientCredentials(new Uri(_options.Value.OAuth2.IssuerUrl), audience, _fileUri));
+                _client.CloseAsync();
             }
 
-            return await builder.BuildAsync();
-        }
+            // Large fields to null;
+            _options = null;
+            _admin = null;
+            _credentials = null;
 
-        public async Task<IPulsarAdminRESTAPIClient> GetAdminClientAsync()
-        {
-            if (_admin != null) return _admin;
-
-            var client = new HttpClient
-            {
-                BaseAddress = new Uri($"{_options.Value.AdminUrl}/admin/v2/")
-            };
-
-            if (_options.Value.OAuth2 != null)
-            {
-                var oauth2 = _options.Value.OAuth2;
-                var token = await GetTokenAsync(oauth2.TokenAddress, oauth2.GrantType, oauth2.Audience, _fileUri);
-                client.SetBearerToken(token);
-            }
-
-            return _admin = new PulsarAdminRESTAPIClient(client);
-        }
-
-        public HttpClient GetAdminHttpClient()
-        {
-            return new HttpClient {BaseAddress = new Uri($"{_options.Value.AdminUrl}/admin/v2/")};
-        }
-
-        private static async Task<PulsarOAuthData> ReadOAuth2File(Uri fileUri)
-        {
-            // Load Json
-            var webRequest = WebRequest.Create(fileUri);
-            webRequest.Credentials = CredentialCache.DefaultCredentials;
-            webRequest.Method ="GET";
-            var webResponse = await webRequest.GetResponseAsync();
-            var contents = await new StreamReader(webResponse.GetResponseStream()).ReadToEndAsync();
-            return JsonConvert.DeserializeObject<PulsarOAuthData>(contents);
-        }
-
-        private static async Task<string> GetTokenAsync(string tokenAddress, string grantType, string audience, Uri fileUri)
-        {
-            var json = await ReadOAuth2File(fileUri);
-            var request = new TokenRequest
-            {
-                Address = tokenAddress,
-                GrantType = grantType,
-                ClientId = json.ClientId,
-                ClientSecret = json.ClientSecret,
-                ClientCredentialStyle = ClientCredentialStyle.PostBody,
-            };
-            request.Parameters.Add("audience", audience);
-            request.Parameters.Add("type", json.Type);
-            request.Parameters.Add("client_email", json.ClientEmail);
-            request.Parameters.Add("issuer_url", json.IssuerUrl);
-
-            // Get Token
-            var client = new HttpClient();
-            var response = await client.RequestTokenAsync(request);
-            return response.AccessToken;
-        }
-
-        public void Dispose()
-        {
-            File.Delete(_fileName);
+            disposed = true;
         }
     }
 }
